6ee43be66e171789b7f7be812eb1c2e8aa78a7c6
Lahari2312 <24wh1a05ba@bvrithyderabad.edu.in>
2026-07-03 13:43:40 +0530

    initial comit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static("frontend"));
 
-const PORT =  3002;
+const PORT =  process.env.PORT ||3002;
 
 app.listen(PORT, function(){
     console.log("Server Running at http://localhost:" + PORT);
